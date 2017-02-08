1791ed81114c0f4b65d0c7df668bdf7337051c73
ozdemirali <ozdemirali@hotmail.com>
2017-02-09 00:04:18 +0300

    Menu

--- NestedContainers/Dynamic/Js/menu.js
+++ NestedContainers/Dynamic/Js/menu.js
@@ -4,54 +4,42 @@
 var app = angular.module("menu", ["dndLists"]);
 app.controller("menuCtrl", function ($scope) {
 
-    $scope.deneme = "menu çalýþýyor";
+    $scope.deneme = "Deneme çalýþýyor...";
 
     //$scope.buttons=["CALL","SMS","TTT"]
 
     $scope.models = {
         selected: null,
-        buttons:["Save","Cancel","Attach"],
+        buttons: ["Save", "Cancel", "Attach"],
         tempWidgetButtons: ["CALL", "SMS", "TTT"],
         tempEntity: ["Stok", "Personel", "Yonetici"],
         tempShowActionButton: ["true", "false"],
         tempActionButtonLink: ["Form1", "From2", "From3"],
 
         templates: [
-            
-            { type: "button", id: 1, name: "Button", icon: "fa fa-sign-in",buttons:[] },
-            { type: "checkbox", id: 2, name: "Checkbox", icon: "fa fa-check-square-o", buttons: [] },
-            { type: "dateTimePicker", id: 3, name: "DateTimePicker", icon: "fa fa-calendar", buttons: [] },
-            { type: "label", id: 4, name: "Label", icon: "fa fa-font", buttons: [] },
-            { type: "parentId", id: 5, name: "ParentId", icon: "fa fa-id-badge", buttons: [] },
-            { type: "pictureBox", id: 6, name: "PictureBox", icon: "fa fa-picture-o", buttons: [] },
-            { type: "radio", id: 7, name: "RadioButton", icon: "fa fa-dot-circle-o", buttons: [] },
-            { type: "subForm", id: 8, name: "SubFrom", icon: "fa fa-window-maximize", buttons: [] },
-            { type: "text", id: 9, name: "TextBox", icon: "fa fa-align-justify", buttons: [] },
-            { type: "timePicker", id: 10, name: "TimePicker", icon: "fa fa-calendar", buttons: [] },
+
+            { type: "item", id: 1, name: "Item", icon: "fa fa-sign-in"},
+            { type: "menu", id: 2, name: "Menu", icon: "fa fa-check-square-o", columns: [[]] },
+            { type: "list", id: 3, name: "list", icon: "fa fa-sign-in" },
+          
 
         ],
 
         dropzones: {
-            "Form":{},
-            "Buttons":[],
-            "Widget":{"Item": [
-               
-                {
-                    "type": "text",
-                    "id": "9",
-                    "name": "TextBox",
-                    "buttons":[]
-                   
+            "Form": {},
+            "Buttons": [],
+            "Widget": {
+                "Widgets": [
 
-                }
 
-        ]},
+                ]
+            },
 
-           
-       }
+
+        }
     };
 
- /*Dropzone içindeki Buttons Dizisinin içindeki   buttons'larý seçereken kullanýlýyor  */
+    /*Dropzone içindeki Buttons Dizisinin içindeki   buttons'larý seçereken kullanýlýyor  */
     $scope.buttonsSelection = function buttonsSelection(item) {
 
         var idx = $scope.models.dropzones.Buttons.indexOf(item);
@@ -69,7 +57,7 @@ app.controller("menuCtrl", function ($scope) {
 
     /**********************************************************************/
 
-/*Dropzone içindeki Widget Dizisinin içindeki   buttons'larý seçereken kullanýlýyor  */
+    /*Dropzone içindeki Widget Dizisinin içindeki   buttons'larý seçereken kullanýlýyor  */
     $scope.widgetButtonsSelection = function widgetButtonsSelection(item) {
 
         var idx = $scope.models.selected.buttons.indexOf(item);
@@ -85,8 +73,8 @@ app.controller("menuCtrl", function ($scope) {
         }
     };
 
-/**********************************************************************/
-    $scope.$watch('models.dropzones', function(model) {
+    /**********************************************************************/
+    $scope.$watch('models.dropzones', function (model) {
         $scope.modelAsJson = angular.toJson(model, true);
     }, true);
 
